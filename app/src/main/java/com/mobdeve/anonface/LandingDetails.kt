a7773b4d57ce81165ebca07da267e6288fb5b6c7
Kathleen Mae Romblon <kathleen_romblon@dlsu.edu.ph>
2024-10-08 01:40:34 +0800

    created landing page + update logo

---
+++ app/src/main/java/com/mobdeve/anonface/LandingDetails.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.anonface
+
+class LandingDetails(background: Int, flairText: Int) {
+
+    var background = background
+        private set
+
+    var flairText = flairText
+        private set
+
+}
--- app/src/main/java/com/mobdeve/anonface/MainActivity.kt
+++ app/src/main/java/com/mobdeve/anonface/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
@@ -12,6 +13,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +26,33 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //Recycler image slider
+        var landingSlider : ArrayList<LandingDetails> = arrayListOf(
+            LandingDetails(R.drawable.landing_1,R.string.landing_text1),
+            LandingDetails(R.drawable.landing_2,R.string.landing_text2),
+            LandingDetails(R.drawable.landing_3,R.string.landing_text3)
+        )
+
+        var landingRecycler : RecyclerView = findViewById(R.id.landingRecycler)
+        landingRecycler.adapter = LandingAdapter(landingSlider)
+        landingRecycler.layoutManager = LinearLayoutManager(this,
+            LinearLayoutManager.HORIZONTAL, false)
+        PagerSnapHelper().attachToRecyclerView(landingRecycler)
+        landingRecycler.addOnScrollListener(PageIndicator(this))
+
+        var currPos : Int = 0
+        var button : Button = findViewById(R.id.startPhotoCaptureActivityBtn)
+        button.setOnClickListener {
+            currPos = (landingRecycler.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+            landingRecycler.smoothScrollToPosition(currPos+1)
+        }
+
+        if (currPos < 2) {
+            button.visibility = View.GONE
+        } else {
+            button.visibility = View.VISIBLE
         }
 
         if (!permissionsGranted()) {
